7ab36fd3de34d8e32d225c64f2d270b8b0f45b95
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2025-04-22 01:01:09 +0200

    feat: add smartd

--- monitoring.nix
+++ monitoring.nix
@@ -11,4 +11,15 @@
   };
 
   networking.firewall.allowedTCPPorts = [ 19999 ];
+
+  services.smartd = {
+    # Self note: the USB drive is not supported by smartd - shuck it asap
+    enable = true;
+    extraOptions = [
+      "-A /var/log/smartd/"
+      "--interval=3600"
+    ];
+    # Short tests every 7 days, long tests every month
+    defaults.monitored = "-a -n standby,10,q -o on -s (S/../../7/02|L/../01/./04) -m vuks";
+  };
 }
