f274eb38aa92de230d8e8f13f7b9c410745a2892
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2025-05-03 00:42:48 +0200

    fix: open listen port for qbittorrent

--- qbittorrent.nix
+++ qbittorrent.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 let
-  qbittorrentPort = 53246;
+  webUiPort = 53246;
+  listenPort = 51212;
 in
 {
   # NOTE: in order for qbit to work with proxymanager, you need to
@@ -18,12 +19,12 @@ in
       User = "qbittorrent";
       Group = "qbittorrent";
       WorkingDirectory = "/home/qbittorrent";
-      ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox --webui-port=${builtins.toString qbittorrentPort} --confirm-legal-notice";
+      ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox --webui-port=${builtins.toString webUiPort} --confirm-legal-notice";
       Restart = "always";
       RestartSec = "30";
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ qbittorrentPort ];
-  networking.firewall.allowedUDPPorts = [ qbittorrentPort ];
+  networking.firewall.allowedTCPPorts = [ webUiPort listenPort ];
+  networking.firewall.allowedUDPPorts = [ webUiPort listenPort ];
 }
