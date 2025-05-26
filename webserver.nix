912fe7ecc4dd59bc2269702b5c2f23b62a3b81c1
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2025-05-26 20:41:23 +0200

    chore: reorganize nginx virtual hosts for clarity and consistency

--- webserver.nix
+++ webserver.nix
@@ -7,48 +7,52 @@ in
 {
   services.nginx = {
     enable = true;
-    virtualHosts."media.vuks69.duckdns.org" = {
-      # basicAuthFile = "/var/www/.htpasswd";
-      root = "/var/www/media/";
-      listen = [
-        {
-          addr = "127.0.0.1";
-          port = fileHostingPort;
-        }
-      ];
-      locations."/" = {};
-    };
-    virtualHosts."site.vuks-den.duckdns.org" = {
-      root = "/var/www/staging/";
-      listen = [
-        {
-          addr = "127.0.0.1";
-          port = localWebsitePort;
-        }
-      ];
-      locations."/" = {
+    virtualHosts = {
+      # Production
+      "vuks.dev" = {
+        root = "/var/www/website/";
+        listen = [
+          {
+            addr = "127.0.0.1";
+            port = publicWebsitePort;
+          }
+        ];
+        locations."/" = { };
+      };
+      "media.vuks.dev" = {
+        root = "/var/www/media/";
+        listen = [
+          {
+            addr = "127.0.0.1";
+            port = fileHostingPort;
+          }
+        ];
+        locations."/" = { };
+      };
+
+      # Staging
+      "site.vuks-den.duckdns.org" = {
+        root = "/var/www/staging/";
+        listen = [
+          {
+            addr = "127.0.0.1";
+            port = localWebsitePort;
+          }
+        ];
+        locations."/" = { };
+      };
+      "media.vuks69.duckdns.org" = {
+        # basicAuthFile = "/var/www/.htpasswd";
+        root = "/var/www/media/";
+        listen = [
+          {
+            addr = "127.0.0.1";
+            port = fileHostingPort;
+          }
+        ];
+        locations."/" = { };
       };
     };
 
-    virtualHosts."vuks.dev" = {
-      root = "/var/www/website/";
-      listen = [
-        {
-          addr = "127.0.0.1";
-          port = publicWebsitePort;
-        }
-      ];
-      locations."/" = {};
-    };
-    virtualHosts."media.vuks.dev" = {
-      root = "/var/www/media/";
-      listen = [
-        {
-          addr = "127.0.0.1";
-          port = fileHostingPort;
-        }
-      ];
-      locations."/" = {};
-    };
   };
 }
