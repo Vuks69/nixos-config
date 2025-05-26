31540232350ca0649ff7d017477deb3ad3a555cf
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2025-05-26 18:32:00 +0200

    feat: add additional virtual hosts and ports for nginx configuration

--- webserver.nix
+++ webserver.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 let
   fileHostingPort = 58383;
+  localWebsitePort = 58384;
+  publicWebsitePort = 58384;
 in
 {
   services.nginx = {
@@ -8,20 +10,45 @@ in
     virtualHosts."media.vuks69.duckdns.org" = {
       # basicAuthFile = "/var/www/.htpasswd";
       root = "/var/www/media/";
-      extraConfig = ''
-        autoindex on;
-      '';
       listen = [
         {
           addr = "127.0.0.1";
           port = fileHostingPort;
         }
       ];
+      locations."/" = {};
+    };
+    virtualHosts."site.vuks-den.duckdns.org" = {
+      root = "/var/www/staging/";
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = localWebsitePort;
+        }
+      ];
       locations."/" = {
-        extraConfig = ''
-          autoindex on;
-        '';
       };
+    };
+
+    virtualHosts."vuks.dev" = {
+      root = "/var/www/website/";
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = publicWebsitePort;
+        }
+      ];
+      locations."/" = {};
+    };
+    virtualHosts."media.vuks.dev" = {
+      root = "/var/www/media/";
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = fileHostingPort;
+        }
+      ];
+      locations."/" = {};
     };
   };
 }
