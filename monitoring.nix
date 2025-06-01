5f5c6fc082b15c529b3ba0266cc2f28409b57a7e
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2025-06-01 23:03:55 +0200

    feat: add zabbix server

--- monitoring.nix
+++ monitoring.nix
@@ -1,4 +1,8 @@
 { pkgs, ... }:
+let
+  zabbixServerListenPort = 10051;
+  zabbixWebPort = 13451;
+in
 {
   services.netdata = {
     enable = true;
@@ -11,6 +15,46 @@
   };
 
   networking.firewall.allowedTCPPorts = [ 19999 ];
+
+  services.zabbixServer = {
+    enable = true;
+    settings = {
+      CacheSize = "1G";
+    };
+    listen.port = zabbixServerListenPort;
+    openFirewall = true;
+  };
+
+  # For Windows hosts, you need to manually open the firewall ports for Zabbix:
+  # netsh advfirewall firewall add rule name="Open Zabbix agentd port 10050 inbound" dir=in action=allow protocol=TCP localport=10050
+  # netsh advfirewall firewall add rule name="Open Zabbix agentd port 10050 outbound" dir=out action=allow protocol=TCP localport=10050
+  # netsh advfirewall firewall add rule name="Open Zabbix trapper port 10051 inbound" dir=in action=allow protocol=TCP localport=10051
+  # netsh advfirewall firewall add rule name="Open Zabbix trapper port 10051 outbound" dir=out action=allow protocol=TCP localport=10051
+  services.zabbixAgent = {
+    enable = true;
+    server = "127.0.0.1";
+  };
+
+  services.zabbixWeb = {
+    enable = true;
+    hostname = "zabbix.vuks-den.duckdns.org";
+    server = {
+      address = "127.0.0.1";
+      port = zabbixServerListenPort;
+    };
+    frontend = "nginx";
+    nginx.virtualHost = {
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = zabbixWebPort;
+        }
+      ];
+    };
+  };
+
+  # Zabbix does not support PHP 8.4 yet, so we force 8.3
+  services.phpfpm.phpPackage = pkgs.php83;
 
   services.smartd = {
     # Self note: the USB drive is not supported by smartd - shuck it asap
