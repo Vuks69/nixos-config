96324f8afc7dbbe7c60ced6e3de306233d8f6548
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2026-01-11 19:19:59 +0100

    refactor: rewrite qBittorrent service configuration to use existing modules

--- modules/services/qbittorrent.nix
+++ modules/services/qbittorrent.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 let
   webUiPort = 53246;
   listenPort = 51212;
@@ -6,27 +6,11 @@ in
 {
   # NOTE: in order for qbit to work with proxymanager, you need to
   #       first login to webui using localhost, and disable CSRF protection
-  environment.systemPackages = with pkgs; [
-    qbittorrent-nox
-  ];
-
-  systemd.services.qbittorrent = {
+  services.qbittorrent = {
     enable = true;
-    description = "qBittorrent client";
-    after = [ "network-online.target" ];
-    requires = [ "network-online.target" ];
-    serviceConfig = {
-      User = "qbittorrent";
-      Group = "qbittorrent";
-      StateDirectory = "qbittorrent";
-      WorkingDirectory = "/var/lib/qbittorrent";
-      Environment = "HOME=/var/lib/qbittorrent";
-      ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox --webui-port=${builtins.toString webUiPort} --confirm-legal-notice";
-      Restart = "always";
-      RestartSec = "30";
-    };
+    webuiPort = webUiPort;
+    torrentingPort = listenPort;
+    openFirewall = true;
+    extraArgs = [ "--confirm-legal-notice" ];
   };
-
-  networking.firewall.allowedTCPPorts = [ webUiPort listenPort ];
-  networking.firewall.allowedUDPPorts = [ webUiPort listenPort ];
 }
