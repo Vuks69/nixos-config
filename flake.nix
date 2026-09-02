ca15041aa794756f2763384cbfaca43f90b4b030
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2026-09-02 19:43:03 +0200

    chore: switch to nixos 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration (flake)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable"; # Pretty much same thing as nixpkgs-unstable, but a few days later, with NixOS tests
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     agenix.url = "github:ryantm/agenix";
