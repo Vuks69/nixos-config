59cfd24f7107cb1e8d8786c88161184644ff493f
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2026-09-18 17:10:03 +0200

    config: lower max scheduler interval to 4h

--- modules/services/miniflux.nix
+++ modules/services/miniflux.nix
@@ -12,6 +12,7 @@ in
       LISTEN_ADDR = "127.0.0.1:${listenAddr}";
       POLLING_FREQUENCY = "30";
       POLLING_SCHEDULER = "entry_frequency";
+      SCHEDULER_ENTRY_FREQUENCY_MAX_INTERVAL = "240";
       FETCHER_ALLOW_PRIVATE_NETWORKS = 1;
     };
   };
